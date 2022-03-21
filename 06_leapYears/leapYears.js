809bc42b60443cd61010d1077c57571e9f360799
Joseph Kessler <kesslerjx@icloud.com>
2022-03-21 12:25:09 -0400

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    
+    if (year%100==0 && year%400==0) { //if divisible by 100 and 400 then leap year
+        return true;
+    } else if(year%100==0 && year%400!=0){ //if divisible by 100 and not 400 then not leap year
+        return false;
+    } else if(year%4 == 0) {//if divisible by 4 then leap year
+        return true;
+    } else { //else then false
+        return false; 
+    } 
 
 };
 
