b0ab5f5c6868fc6233db5f4d4eccd0ef1e9fb28b
Joseph Kessler <kesslerjx@icloud.com>
2022-03-21 12:30:53 -0400

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let converted = (temperature - 32) * (5/9);
+  return Math.round(converted*10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let converted = (temperature * (9/5)) + 32;
+  return Math.round(converted*10) / 10;
 };
 
 // Do not edit below this line
