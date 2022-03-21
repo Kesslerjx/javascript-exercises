4455f687c10d8bdc89405aafddf8213faa380add
Joseph Kessler <kesslerjx@icloud.com>
2022-03-21 10:54:50 -0400

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let sum = 0;
+
+    //Check for negatives and non number types
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    } else {
+
+        //Sum the numbers starting at the smallest number and working up
+        if (start < end) {
+            for(i = start; i <= end; i++){
+                sum += i
+            }
+        } else {
+            for(i = end; i <= start; i++){
+                sum += i
+            }
+        }
+    }
+
+    return sum;
 
 };
 
