d200cd16791bf9e5e5a5fd702943a32700a2dd7a
Joseph Kessler <kesslerjx@icloud.com>
2022-03-25 09:48:37 -0400

    Finish lesson 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,55 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
 const sum = function() {
-	
+	let sum = 0;
+  let array = arguments[0];
+
+  for(i=0;i<array.length;i++){
+    sum += array[i];
+  }
+
+  return sum;
 };
 
 const multiply = function() {
+  let sum = 1;
+  let array = arguments[0];
 
+  for(i=0;i<array.length;i++){
+    sum *= array[i];
+  }
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let answer = number;
+
+  for(i=1;i<power;i++){
+    answer *= number;
+  }
+
+  return answer;
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if (number == 0) {
+    return 1
+  } else {
+    let answer = number;
+
+    for(i=number-1;i>0;i--){
+      answer *= i;
+    }
+  
+    return answer;
+  }
 	
 };
 
